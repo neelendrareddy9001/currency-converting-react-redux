dcfa1837fd3d5034aefc7b834577c17ada64ea94
neelendrarerddy9001 <neelendrareddy123@gmail.com>
2024-12-03 23:37:06 +0100

    styles of this project and currencyPicker ui

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import "./App.css";
 import ExchangeRate from "./components/ExchangeRate";
 
 function App() {
--- src/components/CurrencyCodePicker.js
+++ src/components/CurrencyCodePicker.js
@@ -1,7 +1,19 @@
 import React from "react";
 
-const CurrencyCodePicker = () => {
-  return <div>CurrencyCodePicker</div>;
+const CurrencyCodePicker = ({
+  supportedCurrencies,
+  currencyCode,
+  onChange,
+}) => {
+  return (
+    <select className="currencyCode" value={currencyCode} onChange={onChange}>
+      {supportedCurrencies.map((code) => (
+        <option key={code} value={code}>
+          {code}
+        </option>
+      ))}
+    </select>
+  );
 };
 
 export default CurrencyCodePicker;
