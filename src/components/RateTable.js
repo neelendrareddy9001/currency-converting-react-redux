3f931a3f993a024110537deb1976bb17db093cd1
neelendrarerddy9001 <neelendrareddy123@gmail.com>
2024-12-05 23:52:42 +0100

    reducer files and store added

--- src/components/RateTable.js
+++ src/components/RateTable.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const RateTable = () => {
-  return <div>RateTable</div>;
+  const amount = useSelector();
+  const reates = useSelector();
+  return (
+    <table className="ExchangeRate-table">
+      <tbody></tbody>
+    </table>
+  );
 };
 
 export default RateTable;
---
+++ src/store.js
@@ -0,0 +1,12 @@
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { thunk } from "redux-thunk";
+
+import { rateReducer } from "./reducers/RateReducer";
+import { userReducer } from "./reducers/UserReducer";
+
+const rootReducer = combineReducers({
+  rate: rateReducer,
+  user: userReducer,
+});
+
+export const store = createStore(rootReducer, applyMiddleware(thunk));
