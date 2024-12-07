6059fb6351f61dafcf6e2ebd409ae3f2dcbf35a3
neelendrarerddy9001 <neelendrareddy123@gmail.com>
2024-12-07 23:39:59 +0100

    connected to api

--- src/api.js
+++ src/api.js
@@ -1,8 +1,15 @@
-export function getExchangeRates(base, supportCurrencies) {
-  const symbol = supportCurrencies.filter((symbol) => symbol !== base).join();
+export function getExchangeRates(base, supportedSymbols) {
+  const symbols = supportedSymbols
+    .filter((symbol) => symbol !== base) // exclude your own code from requested symbols
+    .join();
+  const url = `http://api.exchangeratesapi.io/latest?base=${base}&symbols=${symbols}`;
+  return fetch(url)
+    .then((res) => res.json())
+    .then(handleAPIErrors)
+    .then((res) => res.rates);
 }
 
-function handleAPIError(res) {
+function handleAPIErrors(res) {
   if (res.success) return res;
   console.log(`Server Error: ${res.error.info}`);
   return {
