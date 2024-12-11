be68d1c3ade44922a9bd79f526edfcd4bb1125b8
neelendrarerddy9001 <neelendrareddy123@gmail.com>
2024-12-11 23:15:21 +0100

    get the data from reducer

--- src/components/RateTable.js
+++ src/components/RateTable.js
@@ -1,12 +1,29 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { getRates } from "../reducers/RateReducer";
 
 const RateTable = () => {
   const amount = useSelector();
-  const reates = useSelector();
+  const rates = useSelector(getRates);
   return (
     <table className="ExchangeRate-table">
-      <tbody></tbody>
+      <tbody>
+        {Object.entries(rates).map(([code, rate]) => {
+          // NOTE: normally avoid floating point math in JS
+          const exchangeAmount = amount * rate || 0.0;
+          return (
+            <tr key={code}>
+              <td>{code}</td>
+              <td>
+                {exchangeAmount.toLocaleString("en", {
+                  style: "currency",
+                  currency: code,
+                })}
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
     </table>
   );
 };
